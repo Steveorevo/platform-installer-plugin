9411ea21e134fba1b8938cb57fb4c327787d8187
Stephen J. Carnam <steve@steveorevo.com>
2016-03-01 18:18:00 -0800

    Testing.

--- src/PlatformInstaller.php
+++ src/PlatformInstaller.php
@@ -5,48 +5,10 @@ namespace Steveorevo\Composer;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
-
-// Our native trace function for PHP
-function trace($msg, $j = false){
-    if (! is_string($msg) && $j===false ){
-        $msg = "(" . gettype($msg) . ") " . var_export($msg, true);
-    }else{
-        if ($j===false) {
-            $msg = "(" . gettype($msg) . ") " . $msg;
-        }
-    }
-    $h = @fopen('http://127.0.0.1:8189/trace?m='.substr(rawurlencode($msg),0,2000),'r');
-    if ($h !== FALSE){
-        fclose($h);
-    }
-}
-
 class PlatformInstaller extends LibraryInstaller
 {
     /**
-     * Obtain any listed platform-
-     */
-    public function getInstallPath(PackageInterface $package)
-    {
-        $pinstaller = false;
-        if ( $this->composer->getPackage() ) {
-            $topExtra = $this->composer->getPackage()->getExtra();
-            if ( !empty( $topExtra['platform-installer'] ) ) {
-                $pinstaller = $topExtra['platform-installer'];
-                trace( "topExtra" );
-            }
-        }
-        $extra = $package->getExtra();
-        if ( !empty( $extra['platform-installer'] ) ) {
-            $pinstaller = $extra['platform-installer'];
-            trace( "extra" );
-        }
-        trace( $pinstaller );
-        return 'something';
-    }
-
-    /**
-     *
+     *  Supported when including the platfrom-installer.
      */
     public function supports($packageType)
     {
--- src/PlatformInstallerPlugin.php
+++ src/PlatformInstallerPlugin.php
@@ -6,11 +6,33 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
+// Our native trace function for PHP
+function trace($msg, $j = false) {
+    if (! is_string($msg) && $j===false ){
+        $msg = "(" . gettype($msg) . ") " . var_export($msg, true);
+    }else{
+        if ($j===false) {
+            $msg = "(" . gettype($msg) . ") " . $msg;
+        }
+    }
+    $h = @fopen('http://127.0.0.1:8189/trace?m='.substr(rawurlencode($msg),0,2000),'r');
+    if ($h !== FALSE){
+        fclose($h);
+    }
+}
+
 class PlatformInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new PlatformInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+
+        $pinstaller = false;
+        $extra = $composer->getPackage()->getExtra();
+        if ( !empty( $extra['platform-installer'] ) ) {
+            $pinstaller = $extra['platform-installer'];
+            trace( $pinstaller );
+        }
     }
 }
