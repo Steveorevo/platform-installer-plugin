bdd57f81083bc714799023c8fb064b24ddaea4b1
Stephen J. Carnam <steve@steveorevo.com>
2016-03-01 17:38:56 -0800

    Testing

--- src/PlatformInstaller.php
+++ src/PlatformInstaller.php
@@ -5,14 +5,35 @@ namespace Steveorevo\Composer;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
+
+// Our native trace function for PHP
+function trace($msg, $j = false){
+    if (! is_string($msg) && $j===false ){
+        $msg = "(" . gettype($msg) . ") " . var_export($msg, true);
+    }else{
+        if ($j===false) {
+            $msg = "(" . gettype($msg) . ") " . $msg;
+        }
+    }
+    $h = @fopen('http://127.0.0.1:8189/trace?m='.substr(rawurlencode($msg),0,2000),'r');
+    if ($h !== FALSE){
+        fclose($h);
+    }
+}
+
 class PlatformInstaller extends LibraryInstaller
 {
     /**
-     * This changed again
+     * Obtain any listed platform-
      */
     public function getInstallPath(PackageInterface $package)
     {
-        return './../someplace';
+        $extra = $package->getExtra();
+        if (! empty( $extra['platform-installer'] ) {
+            $pinstall = $extra['platform-installer'];
+            trace( $pinstall );
+        }
+        return 'vendor';
     }
 
     /**
