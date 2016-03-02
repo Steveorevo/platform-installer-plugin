c6abb7523e25f4f113d3e9d62af5e6682979c57b
Stephen J. Carnam <steve@steveorevo.com>
2016-03-02 00:09:01 -0800

    Remove trace/debug code.

--- src/PlatformInstallerPlugin.php
+++ src/PlatformInstallerPlugin.php
@@ -9,21 +9,6 @@ use Composer\Package\Package;
 use Composer\IO\IOInterface;
 use Composer\Composer;
 
-// Our native trace function for PHP
-function trace($msg, $j = false) {
-    if (! is_string($msg) && $j===false ){
-        $msg = "(" . gettype($msg) . ") " . var_export($msg, true);
-    }else{
-        if ($j===false) {
-            $msg = "(" . gettype($msg) . ") " . $msg;
-        }
-    }
-    $h = @fopen('http://127.0.0.1:8189/trace?m='.substr(rawurlencode($msg),0,2000),'r');
-    if ($h !== FALSE){
-        fclose($h);
-    }
-}
-
 class PlatformInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
@@ -87,7 +72,6 @@ class PlatformInstallerPlugin implements PluginInterface
                 }
             }
         }
-        trace( $installNow );
 
         // Download platform installers
         foreach($installNow as $install) {
